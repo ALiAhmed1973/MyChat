8c587e695aa56f9fb54a5d7d4f71bc6667f25b0d
Ali <eng_aliahmed1973@yahoo.com>
2020-10-11 20:14:24 +0200

    Create SignUpFragment

--- app/src/main/java/com/projects/mychat/LoginFragment.java
+++ app/src/main/java/com/projects/mychat/LoginFragment.java
@@ -3,30 +3,35 @@ package com.projects.mychat;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.projects.mychat.databinding.FragmentLoginBinding;
+
 
 public class LoginFragment extends Fragment {
 
-
+FragmentLoginBinding binding;
     public LoginFragment() {
         // Required empty public constructor
-    }
-
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+       // return inflater.inflate(R.layout.fragment_login, container, false);
+        binding = FragmentLoginBinding.inflate(inflater,container,false);
+        binding.textViewSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(LoginFragmentDirections.actionLoginFragmentToSignUpFragment());
+
+            }
+        });
+        return binding.getRoot();
     }
 }
