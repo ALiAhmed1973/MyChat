2bd92a831c3bfa8ab6584d0f9fc3c678eacf6c59
Ali <eng_aliahmed1973@yahoo.com>
2020-10-12 17:57:05 +0200

    Create PrivateChatFragment

---
+++ app/src/main/java/com/projects/mychat/PrivateChatFragment.java
@@ -0,0 +1,29 @@
+package com.projects.mychat;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PrivateChatFragment extends Fragment {
+
+
+
+    public PrivateChatFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_private_chat, container, false);
+    }
+}
