59558c7756aeff0be7418861fdb39aa97d5d9131
Ali <eng_aliahmed1973@yahoo.com>
2020-10-12 17:43:37 +0200

    Create navDrawer

--- app/src/main/java/com/projects/mychat/MainActivity.java
+++ app/src/main/java/com/projects/mychat/MainActivity.java
@@ -1,14 +1,42 @@
 package com.projects.mychat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+import com.projects.mychat.databinding.ActivityMainBinding;
 
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+ActivityMainBinding binding;
+    NavHostFragment navHostFragment;
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+         navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.myNavHostFragment);
+         navController = Objects.requireNonNull(navHostFragment).getNavController();
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(navController.getGraph()).setOpenableLayout(binding.drawerLayout).build();
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView,navController);
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController,binding.drawerLayout);
+    }
+
+
 }
