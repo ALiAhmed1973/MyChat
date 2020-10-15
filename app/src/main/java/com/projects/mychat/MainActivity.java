6f6561b8f401ae40314044005454abbbc340bbfb
Ali <eng_aliahmed1973@yahoo.com>
2020-10-15 16:47:25 +0200

    Fix Drawer layout

--- app/src/main/java/com/projects/mychat/MainActivity.java
+++ app/src/main/java/com/projects/mychat/MainActivity.java
@@ -1,8 +1,11 @@
 package com.projects.mychat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -10,6 +13,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.navigation.NavigationView;
 import com.projects.mychat.databinding.ActivityMainBinding;
@@ -31,6 +35,22 @@ public class MainActivity extends AppCompatActivity {
                 new AppBarConfiguration.Builder(navController.getGraph()).setOpenableLayout(binding.drawerLayout).build();
         NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView,navController);
+
+
+        // prevent nav gesture if not on start destination
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                if(destination.getId()==R.id.loginFragment||destination.getId()==R.id.signUpFragment)
+                {
+                    binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+                }else
+                {
+                    binding.drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+                }
+
+            }
+        });
     }
 
     @Override
