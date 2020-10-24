0d7f3b4fae504fe5d799e4bc9d7b70881a0e3d47
Ali <eng_aliahmed1973@yahoo.com>
2020-10-24 17:21:02 +0200

    Add Login ProgressBar

--- app/src/main/java/com/projects/mychat/LoginFragment.java
+++ app/src/main/java/com/projects/mychat/LoginFragment.java
@@ -2,33 +2,27 @@ package com.projects.mychat;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import com.projects.mychat.databinding.FragmentLoginBinding;
 
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
-import sdk.chat.core.dao.User;
 import sdk.chat.core.session.ChatSDK;
 import sdk.chat.core.types.AccountDetails;
-import sdk.chat.core.types.ConnectionType;
 import sdk.guru.common.RX;
 
 
 public class LoginFragment extends Fragment {
 
-FragmentLoginBinding binding;
+private FragmentLoginBinding binding;
+private String email;
+private String password;
     public LoginFragment() {
         // Required empty public constructor
     }
@@ -45,36 +39,26 @@ FragmentLoginBinding binding;
 
         binding.buttonLogin.setOnClickListener(view -> {
 
-            String email = binding.editTextEmailAddress.getText().toString();
-            String password = binding.editTextPassword.getText().toString();
+             email = binding.editTextEmailAddress.getText().toString();
+             password = binding.editTextPassword.getText().toString();
+            if(!checkingFields())
+            {
+                return ;
+            }
             AccountDetails details = AccountDetails.username(email, password);
+            binding.loginProgressbar.setVisibility(View.VISIBLE);
             ChatSDK.auth().authenticate(details)
                     .observeOn(RX.main())
                     .subscribe(()-> {
                         Toast.makeText(view.getContext(),"Login",Toast.LENGTH_SHORT).show();
+                        binding.loginProgressbar.setVisibility(View.INVISIBLE);
                           Navigation.findNavController(view).navigate(LoginFragmentDirections.actionLoginFragmentToSingleChatFragment());
                     }, throwable -> {
+                        binding.loginProgressbar.setVisibility(View.INVISIBLE);
                         Toast.makeText(view.getContext(),throwable.toString(),Toast.LENGTH_LONG).show();
                     });
         });
 
-//        binding.buttonLogout.setOnClickListener(view -> {
-////            ChatSDK.auth().logout()
-////                    .observeOn(RX.main())
-////                    .subscribe(()->Log.d("logout","Logout"),
-////                            t->Log.e("login Not working", t.toString()));
-//
-//            ChatSDK.search().usersForIndex("eng@j.com").observeOn(RX.main()).subscribe(user -> {
-//                        Log.d("Added notworking", user.getName());
-//            }
-//            , t->Log.d("Added notworking", t.toString()));
-////
-//// A               dd contact
-////                ChatSDK.contact().addContact(user, ConnectionType.Contact).observeOn(RX.main()).subscribe(() -> {
-////                    // Contact added
-////                    Toast.makeText(view.getContext(),"Contact Added",Toast.LENGTH_LONG).show();
-////                }, T->Log.d("Added notworking", T.toString()));
-//        });
 
 
         if (ChatSDK.auth().isAuthenticated()) {
@@ -85,13 +69,22 @@ FragmentLoginBinding binding;
         }
         if(ChatSDK.auth().isAuthenticatedThisSession())
         {
+
             Log.d("isAuthenticatedSession","yes AuthenticatedThisSession");
         }else
         {
             Log.d("isAuthenticatedSession","No AuthenticatedThisSession");
         }
         return binding.getRoot();
+    }
 
-
+    private boolean checkingFields()
+    {
+        if(email.isEmpty()||password.isEmpty())
+        {
+            Toast.makeText(getContext(),"One of your fields is empty",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
